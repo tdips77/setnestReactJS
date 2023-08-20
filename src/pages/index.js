b94c278776c08714588e5297cdeb3565e311a62d
jheel1506 <jheel1506@gmail.com>
2023-08-20 12:24:26 +0530

    Initail Change

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,11 +21,7 @@ export default function Home() {
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            
               By{' '}
               <Image
                 src="/vercel.svg"
@@ -35,7 +31,6 @@ export default function Home() {
                 height={24}
                 priority
               />
-            </a>
           </div>
         </div>
 
