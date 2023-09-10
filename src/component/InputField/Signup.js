c76510955d03df4e51b816b029d37cb9ae707c00
Vikas <vikas.pandey@eroute.in>
2023-09-10 20:15:54 +0530

    wip

---
+++ src/component/InputField/Signup.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import styles from '@/styles/Home.module.css';
+import  setnestlogo from '../../../public/assets/Setnest-copy.png';
+import Image from 'next/image'
+import Carousel from 'react-bootstrap/Carousel';
+import crslimg from '../../../public/assets/crslimg.png';
+import OTPInput,  {ResendOTP } from "otp-input-react";
+
+
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6),
+  confpassword: yup.string().required().min(6),
+});
+
+const SignUp = () => {
+    const [OTP, setOTP] = useState("");
+    const [showOTP, setshowOTP] = useState(false);
+    const { register, handleSubmit, setError, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+      });
+      const router = useRouter();
+      const goBack = () => {
+        router.back();
+      };
+      const onSubmit = async (data) => {
+        // Your registration logic
+        // Redirect user after successful registration
+        router.push('/login');
+      };
+
+      const renderButton = (buttonProps) => {
+        return  <button className='resendBtn' {...buttonProps}> Resend the code</button>;
+      };
+      const renderTime = (remainingTime) => {
+        return <span>{remainingTime} sec</span>;
+      };
+
+  return (
+    <div className='container main-set '>
+      <button type='button' className={`${styles.iconBtn}`} onClick={goBack}>
+        {/* <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> */}
+        <FontAwesomeIcon icon={faArrowLeft} className={`${styles.iconleftBtn}`}/>
+        </button>
+        <Image src={setnestlogo} alt='logo' className={'img-fluid ' + styles.topLogo}/>
+        <div className='container'>
+            <div className='row side-padding'>
+                <div className='col-6'>
+                <form onSubmit={handleSubmit(onSubmit)} className='mb-3'>
+                    <h5 className='mb-4'>Sign up</h5>
+        <div>
+
+        <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" {...register('email')}/>
+      </FloatingLabel>
+      <p>{errors.email?.message}</p>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" {...register('password')}/>
+      </FloatingLabel>
+      <p>{errors.password?.message}</p>
+      <FloatingLabel controlId="floatingPassword2" label="Confirm Password">
+        <Form.Control type="password" placeholder="Password" {...register('confpassword')}/>
+      </FloatingLabel>
+      <p>{errors.confpassword?.message}</p>
+
+      <Form.Check
+            label="I agree to all the Term & Condition and Privacy Policy"
+            name="group1"
+            type="checkbox"
+            
+          />
+        </div>
+        {showOTP && 
+
+        
+            <div className='mt-4 text-center'>
+              <p>We have sent the verification OTP to your email address.</p>
+              <p>@emailid</p>
+            <OTPInput className='otpInput' value={OTP} onChange={setOTP} autoFocus OTPLength={6} otpType="number" disabled={false} secure />
+            <div className='d-flex justify-content-center resendDiv mt-2'>
+            Didn't get it?<ResendOTP className='resendOtp' renderButton={renderButton} renderTime={renderTime} onResendClick={() => console.log("Resend clicked")} />
+            </div>
+            </div>
+        
+        }
+        
+        <button type="button" className='signup-btn' onClick={(e) => setshowOTP(true)} >Sign Up</button>
+        
+      </form>
+                </div>
+                <div className='col-6'>
+                <Carousel>
+      <Carousel.Item>
+        <Image src={crslimg} className='img-fluid' text="First slide" />
+        <Carousel.Caption>
+         
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src={crslimg} className='img-fluid' text="Second slide" />
+        <Carousel.Caption>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src={crslimg} className='img-fluid' text="Third slide" />
+        <Carousel.Caption>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+                </div>
+
+            </div>
+
+        </div>
+      
+    </div>
+  );
+};
+
+export default SignUp;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 library.add(fas, fab);
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
